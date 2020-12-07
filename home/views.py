57326647b9c3a4333758002f53d8bf4d9a713293
Burnzy1991 <steveyburns@hotmail.com>
2020-12-07 12:12:21 -0500

    Did a lot on the home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import HomeTest
 # Create your views here.
-def home(request):
-    context = {"a": "1", "b": "2"}
-    return render(request, 'home.html', context)
+
+def testimonial(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = HomeTest(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/aboutme')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = HomeTest()
+
+    return render(request, 'home.html', {'form': form})
